1c73c73c4fc3da17e808dfb213eaf022f3176399
Zach Sisti <zachsisti@gmail.com>
2019-10-09 17:29:24 -0500

    Worked a tiny bit more on Database

--- app/GreenRoutine/app/src/main/java/com/example/greenroutine/DataBaseActivity.java
+++ app/GreenRoutine/app/src/main/java/com/example/greenroutine/DataBaseActivity.java
@@ -1,14 +1,26 @@
 package com.example.greenroutine;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class DataBaseActivity extends AppCompatActivity {
+    private RecyclerView recycView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_base);
+        recycView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        layoutManager = new LinearLayoutManager(this);
+        recycView.setLayoutManager(layoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
