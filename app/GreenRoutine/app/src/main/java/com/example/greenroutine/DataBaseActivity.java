d894ac0f36ece850037178f77ab516e3208db7e4
Zach Sisti <zachsisti@gmail.com>
2019-10-09 17:16:01 -0500

    Started database

---
+++ app/GreenRoutine/app/src/main/java/com/example/greenroutine/DataBaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.greenroutine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataBaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_base);
+    }
+}
