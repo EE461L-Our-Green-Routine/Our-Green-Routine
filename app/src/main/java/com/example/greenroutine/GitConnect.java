56ca8bbe7620124ebf5c3ff88ab06c5bc76735d6
Michael Pontikes <mpontikes@gmail.com>
2019-10-09 19:46:21 -0500

    GitConnect

---
+++ app/src/main/java/com/example/greenroutine/GitConnect.java
@@ -0,0 +1,48 @@
+import org.kohsuke.github.*;
+import java.io.IOException;
+import java.util.List;
+
+/**GitConnect Bridge Class
+ *
+ */
+public class GitConnect {
+
+    static int getGitIssues(String user){
+        int count = -1;
+        try {
+            GitHub gh = GitHub.connectAnonymously();
+            GHRepository ogm = gh.getRepository("mpontikes/Our-Green-Routine");
+            List<GHIssue> mp = ogm.listIssues(GHIssueState.ALL).asList();
+            count = 0;
+            for(GHIssue cmt : mp){
+                if(cmt.getUser().getLogin().equals(user)){
+                    count++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ERR");
+        }
+        return count;
+    }
+    static int getGitCommit(String user){
+        int count = -1;
+        try {
+            GitHub gh = GitHub.connectAnonymously();
+            GHRepository ogm = gh.getRepository("mpontikes/Our-Green-Routine");
+            List<GHCommit> mp = ogm.listCommits().asList();
+            count = 0;
+            for(GHCommit cmt : mp){
+                if(cmt.getAuthor().getLogin().equals(user)){
+                    count++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ERR");
+        }
+        return count;
+    }
+
+    static int getUnitTests(String user){
+        return -1;
+    }
+}
