192e9085ec647519c0dcf7116b1d9a4c87005b76
Hussam Djadi <djadih@utexas.edu>
2019-10-13 01:39:03 -0500

    Initial setup for ItemPage
    Map zooms to hardcoded location (as described in deliverables for phase 1)

--- app/src/main/java/com/example/greenroutine/DataBaseActivity.java
+++ app/src/main/java/com/example/greenroutine/DataBaseActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -49,6 +51,11 @@ public class DataBaseActivity extends AppCompatActivity {
         MyAdapter mAdapter = new MyAdapter(this, cards);
         recycleView.setAdapter(mAdapter);
 
+    }
+
+    protected void sendToItemPage(View view){
+        Intent itemIntent = new Intent(this, ItemPage.class);
+        startActivity(itemIntent);
     }
 
 }
--- app/src/main/java/com/example/greenroutine/MainActivity.java
+++ app/src/main/java/com/example/greenroutine/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToDB(View view){
-        Intent dbIntent = new Intent(this, DataBaseActivity.class);
+        Intent dbIntent = new Intent(this, ItemPage.class);
         startActivity(dbIntent);
     }
 
