37fd82c8e1c5b0139de6269900ac54d875aea32b
ElginAllen <eballen@utexas.edu>
2019-10-13 00:40:22 -0500

    more implementation and xml editting

--- app/src/main/java/com/example/greenroutine/CarbonFootPrint.java
+++ app/src/main/java/com/example/greenroutine/CarbonFootPrint.java
@@ -1,6 +1,9 @@
 package com.example.greenroutine;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,16 @@ public class CarbonFootPrint extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carbon_foot_print);
+    }
+    //gets the user input and calculates the foot print based on that
+    public void calculateFP(View view){
+        TextView inputTextView = findViewById(R.id.carbonInput);
+        String inputString = inputTextView.getText().toString();
+        double value = stringToPoints(inputString);
+    }
+    //does the actual conversion
+    public double stringToPoints(String str){
+        //this is just a hardcoded output for show purpose
+        return 15;
     }
 }
