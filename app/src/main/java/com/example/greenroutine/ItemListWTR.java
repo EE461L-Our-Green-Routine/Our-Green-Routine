fca4f94b2e039c3fcf74a2a41c59f6ec0b2d5b5b
Alex Gonzales <agonzales5071@gmail.com>
2019-10-25 19:36:54 -0500

    Update ItemListWTR.java

--- app/src/main/java/com/example/greenroutine/ItemListWTR.java
+++ app/src/main/java/com/example/greenroutine/ItemListWTR.java
@@ -1,11 +1,28 @@
 package com.example.greenroutine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Ref;
 import java.util.ArrayList;
 
 /* Got information from https://developer.android.com/guide/topics/ui/layout/recyclerview#java */
@@ -13,6 +30,57 @@ public class ItemListWTR extends AppCompatActivity {
     private RecyclerView recycleView;
     private LinearLayoutManager layManager;
     private static final String CATEGORY_NAME = "CATEGORY_NAME";
+    private FirebaseFirestore mFirestore;
+
+
+
+    //gets list of items in input family returns list of cards
+    private ArrayList<Card> getList(final String fam){
+        //mDatabase.child(CATEGORY_NAME).child(itemName);
+        final CollectionReference colRef = mFirestore.collection("Items"); //collection of items from database
+        //DocumentReference docRef = mFirestore.collection("glass").document("glass");
+
+            colRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            ArrayList<Card> itemsInFam = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("nice", document.getId() + " => " + document.getData());
+                                String[] allFams = (String[]) document.get("family_ids");
+                                for(String currentID : allFams){
+                                    if(currentID.equals(fam)){
+                                        String name = (String) document.get("description");
+                                        String descript = (String) document.get("long_description");
+//                                        String picUrl = (String) document.get("image");
+//                                        try {
+//                                            InputStream is = (InputStream) new URL(picUrl).getContent();
+//                                            Drawable itemPic = Drawable.createFromStream(is, null);
+//                                            //not done pic not obtained, card not created or stroed in arraylist
+//                                        } catch (Exception e) {
+//                                            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+//                                        }
+                                        // Card item = new Card();
+                                    }
+                                }
+                            }
+                        } else {
+                            Log.d("notNice", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        //
+//        String[] link = new String[1];
+//        link[0] = "a";
+//        if(link[0].equals("")){
+//            Toast.makeText(getApplicationContext(),"REEEEEE",Toast.LENGTH_SHORT).show();
+//        }
+        //Toast.makeText(getApplicationContext(),link[0],Toast.LENGTH_SHORT).show();
+        TextView linkText = findViewById(R.id.link);
+        //linkText.setText(link);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
