01546ecfde9e75adb6c2a263462952e498b23b15
ElginAllen <eballen@utexas.edu>
2019-11-14 23:06:08 -0600

    simple fix for coolclimate

--- app/src/main/java/com/example/greenroutine/CoolClimate.java
+++ app/src/main/java/com/example/greenroutine/CoolClimate.java
@@ -136,9 +136,9 @@ public class CoolClimate extends AppCompatActivity {
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapterSize = ArrayAdapter.createFromResource(this, R.array.houseSize_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterSize.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        size.setAdapter(adapter);
+        size.setAdapter(adapterSize);
 
     }
 }
