68b417c7bcb1542fb8b4aaf49f34664432b9113f
ElginAllen <eballen@utexas.edu>
2019-11-14 22:59:29 -0600

    Cool Climate up and working

--- app/src/main/java/com/example/greenroutine/CoolClimate.java
+++ app/src/main/java/com/example/greenroutine/CoolClimate.java
@@ -4,26 +4,90 @@ package com.example.greenroutine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoolClimate extends AppCompatActivity {
-    private String apiIncome;
-    private String apiZIP;
-    private String apiSize;
+    private String footPrint = null;
 
-    private int getCarbon(){
-        return 0;
+    private boolean getCarbon(String income, String household, String ZIP){
+        String url = "https://apis.berkeley.edu/coolclimate/footprint-defaults?input_location_mode=1&input_location="
+                + ZIP +"&input_income="+ income +"&input_size="+ household;
+
+        getResponse(url);
+
+        if(footPrint != null) return false;
+
+        return true;
     }
 
     public void calculate(View view){
         Spinner income_spin = (Spinner) findViewById(R.id.incomeSpin);
-        String income = income_spin.getSelectedItem().toString();
         Spinner size_spin = (Spinner) findViewById(R.id.sizeSpin);
-        String houseSize = size_spin.getSelectedItem().toString();
+        EditText txtDescription = (EditText) findViewById(R.id.editText2);
+        String income = getIncome(income_spin.getSelectedItem().toString());
+        String houseSize = getHouseSize(size_spin.getSelectedItem().toString());
+        String ZIP = txtDescription.getText().toString();
+
+        getCarbon(income, houseSize, ZIP);
 
 
+
+    }
+    private void getResponse(String url){
+        String URL = url;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        String l = response.substring(response.indexOf("result_grand_total"));
+                        String actual = l.substring(19, l.indexOf("</result_grand_total"));
+                        TextView myAwesomeTextView = (TextView)findViewById(R.id.textView7);
+                        myAwesomeTextView.setText(actual);
+                        Log.e("Check Response",response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Check Error","Error");
+                    }
+                }
+        ){
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return new byte[]{};
+
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> map = new HashMap<>();
+                map.put("accept", "application/json");
+                map.put("app_id", getString(R.string.coolClimateID));
+                map.put("app_key", getString(R.string.coolClimateKey));
+                return map;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(15000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_MAX_RETRIES));
+        queue.add(request);
     }
     private String getIncome(String input){
         String ret = "1";
